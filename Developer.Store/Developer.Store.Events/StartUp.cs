10976e2da2c768451dfc223d013961a12445f3c3
Bruno Pires <piresbno1992@hotmail.com>
2025-01-20 16:24:24 -0300

    rabbit implementation

---
+++ Developer.Store/Developer.Store.Events/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using Developer.Store.Events.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Rebus.Bus;
+using Rebus.Config;
+using Rebus.Routing.TypeBased;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developer.Store.Events
+{
+    public class StartUp
+    {
+        public static void ConfigureRebus(IServiceCollection services)
+        {
+            services.AddRebus(configure => configure
+                .Transport(t => t.UseRabbitMq("amqp://guest:guest@localhost", "my-queue"))
+                .Routing(r => r.TypeBased().Map<SaleCreatedEvent>("my-queue")));
+            var serviceProvider = services.BuildServiceProvider();
+            var bus = serviceProvider.GetRequiredService<IBus>();
+        }
+        
+    }
+}
---
+++ Developer.Store/Developer.Store.Queue.Worker/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    private async static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = args[1], Password = args[2], UserName = args[3] };
+        using (var connection = factory.CreateConnectionAsync().Result)
+        using (var channel = connection.CreateChannelAsync().Result)
+        {
+            await channel.QueueDeclareAsync(queue: args[0],
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            var message = string.Empty;
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine(message);
+                    if (message != null)
+                    {
+                        using (StreamWriter sw = new StreamWriter(args[4], true, Encoding.ASCII))
+                        {
+                            //Write a line of text
+                            sw.WriteLine(message + "\n");
+                            //Write a second line of text
+                            //Close the file
+                            sw.Close();
+                        }
+                    }
+
+                    channel.BasicAckAsync(ea.DeliveryTag, false);
+                    return null;
+                }
+                catch (Exception ex)
+                {
+                    //Log (Nack para dizer que o consumo da msg não foi possível)
+                    // DelivaryTag -> Carimbo de entrega
+                    // 3° Parametro recoloca ou não a msg na fila novamente
+                    channel.BasicNackAsync(ea.DeliveryTag, false, true);
+                    return null;
+                }
+            };
+
+        }
+    }
+}
